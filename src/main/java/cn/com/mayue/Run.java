5edd2a0841ee618147190d0c01eceaad655293de
mayue100 <982170730@qq.com>
2017-02-15 23:39:29 +0800

    Signed-off-by: mayue100 <982170730@qq.com>

---
+++ src/main/java/cn/com/mayue/Run.java
@@ -0,0 +1,10 @@
+package cn.com.mayue;
+
+/**
+ * Created by Administrator on 2017/2/15.
+ */
+public class Run {
+    public static void main(String[] args) {
+        new Heritrix().instanceMain(args);
+    }
+}
